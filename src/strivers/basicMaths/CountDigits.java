c9946bbd32ef1602a255fb3b83be40b11a63ac2f
hasanmohdkhan <hasanzian@gmail.com>
2024-06-04 16:06:27 +0530

    new problems

---
+++ src/strivers/basicMaths/CountDigits.java
@@ -0,0 +1,6 @@
+package strivers.basicMaths;/**
+* @Auther : hasan.khan
+* @Created : 20-Feb-24
+* @Description : <a href="">link</a>
+*/public class CountDigits {
+}
---
+++ src/strivers/basics/Pattern.java
@@ -0,0 +1,6 @@
+package strivers.basics;/**
+* @Auther : hasan.khan
+* @Created : 19-Feb-24
+* @Description : <a href="">link</a>
+*/public class Pattern {
+}
